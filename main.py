1e2f9e8b15ec1825bdb89996751c785362f3c434
Andy <Andrew.f432@gmail.com>
2015-03-17 22:23:01 +0000

    Picture Upload

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import jinja2
 import os
+import jinja2
+import urllib
 import webapp2
-from google.appengine.api import users
+
 from google.appengine.ext import ndb
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import blobstore_handlers
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -13,8 +19,7 @@ class Comment(ndb.Model):
 class HomePage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        if user == False:
-            self.redirect(users.create_login_url(self.request.uri))
+
             #greeting = ("Hello %s! (<a href ='%s'>Sign Out!</a>)" % (user.nickname(), users.create_logout_url('/')))
             #self.response.write(greeting)
         #else:
@@ -25,28 +30,37 @@ class HomePage(webapp2.RequestHandler):
 
 class BlankPage(webapp2.RequestHandler):
     def get(self):
+        upload_url = blobstore.create_upload_url('/upload')
         qry = Comment.query(Comment.commentId == 1)
         comments = qry.fetch()
         template_values = {
-            "comments" : comments
+            "comments" : comments,
+            "upload_url" : upload_url
         }
         template = jinja_environment.get_template('templates/blank.html')
         self.response.write(template.render(template_values))
 
-        """for self.comment in qry.fetch(10):
-            self.response.write('<p>%s</p>' % self.comment.content)"""
-
     def post(self):
         self.comment = Comment(content=self.request.get('post'), commentId=1)
         self.comment.put()
-        self.redirect('/blank')
 
-class SubForm(webapp2.RequestHandler):
-    def get(self):
-        self.redirect('/blank')
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+        blob_info = upload_files[0]
+        self.redirect('/serve/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
+
+
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
     ('/blank', BlankPage),
-    ('/sub', SubForm)],
+    ('/upload', UploadHandler),
+    ('/serve/([^/]+)?', ServeHandler)],
     debug=True)
