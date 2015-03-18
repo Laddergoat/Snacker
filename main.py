ba23502c0760a3d5ccfc70a139bb3b78dfb9dd3d
Andy <Andrew.f432@gmail.com>
2015-03-18 17:29:14 +0000

    User login through google

--- main.py
+++ main.py
@@ -5,44 +5,55 @@ import webapp2
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
-from google.appengine.ext import webapp
 from google.appengine.ext import blobstore
-from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import blobstore_handlers
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class SnackerImage(ndb.Model):
+    imageUrl = ndb.StringProperty()
+    uploader = ndb.StringProperty()
+    content = ndb.StringProperty()
+
 
 class Comment(ndb.Model):
     content = ndb.StringProperty()
     commentId = ndb.IntegerProperty()
 
+
 class HomePage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.write(template.render())
+        self.response.write(greeting)
 
-            #greeting = ("Hello %s! (<a href ='%s'>Sign Out!</a>)" % (user.nickname(), users.create_logout_url('/')))
-            #self.response.write(greeting)
-        #else:
-
-
-        template= jinja_environment.get_template('templates/index.html')
-        self.response.out.write(template.render())
 
 class BlankPage(webapp2.RequestHandler):
     def get(self):
         upload_url = blobstore.create_upload_url('/upload')
         qry = Comment.query(Comment.commentId == 1)
         comments = qry.fetch()
+        # Variables that should be accessible to the template
         template_values = {
-            "comments" : comments,
-            "upload_url" : upload_url
+            "comments": comments,
+            "upload_url": upload_url
         }
         template = jinja_environment.get_template('templates/blank.html')
+        # Post Template and assign values that are required
         self.response.write(template.render(template_values))
 
     def post(self):
         self.comment = Comment(content=self.request.get('post'), commentId=1)
         self.comment.put()
+
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
@@ -50,12 +61,12 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
         blob_info = upload_files[0]
         self.redirect('/serve/%s' % blob_info.key())
 
+
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, resource):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
         self.send_blob(blob_info)
-
 
 
 app = webapp2.WSGIApplication([
