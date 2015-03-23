599c23d4314606668973cbc97f737f8f5eb7a9f7
Andy <Andrew.f432@gmail.com>
2015-03-23 16:15:16 +0000

    Changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import jinja2
 import urllib
 import webapp2
+from uuid import uuid4
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
@@ -12,18 +13,19 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 
 class User(ndb.Model):
-    index = ndb.StringProperty(required=True, indexed=True)
-    user = ndb.StringProperty(required=True)
+    userid = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    index = ndb.StringProperty(required=True)
     name = ndb.StringProperty(required=True)
+    description = ndb.StringProperty()
     joined = ndb.DateTimeProperty(auto_now_add=True, indexed=False)
-    following = ndb.StringProperty(repeated=True, indexed=False)
-    follower = ndb.StringProperty(repeated=True, indexed=False)
+
 
     @staticmethod
     def getcurrentuser():
         currentuser = users.get_current_user()
         if currentuser:
-            lookup = User.query(User.user == currentuser.email())
+            lookup = User.query(User.userid == currentuser.user_id())
             fetchresult = lookup.fetch()
             for fetchresult in fetchresult:
                 return fetchresult.name
@@ -34,10 +36,20 @@ class User(ndb.Model):
 
 class Image(ndb.Model):
     blob_key = ndb.BlobKeyProperty()
-    user = ndb.StringProperty()
+    user = ndb.StringProperty(required=True)
+    tags = ndb.StringProperty(repeated=True)
+    description = ndb.StringProperty()
     uploaded = ndb.DateTimeProperty(auto_now_add=True)
-    likes = ndb.UserProperty(repeated=True, indexed=False)
-    dislikes = ndb.UserProperty(repeated=True, indexed=False)
+
+
+class Follower(ndb.Model):
+    user = ndb.StringProperty()
+    follower = ndb.StringProperty()
+
+
+class Likes(ndb.Model):
+    image_key = ndb.StringProperty()
+    user = ndb.StringProperty()
 
 
 class HomePage(webapp2.RequestHandler):
@@ -48,21 +60,21 @@ class HomePage(webapp2.RequestHandler):
 
         if currentuser:
             greeting = "<a href='%s'>Log out</a>" % users.create_logout_url('/')
-            qry = User.query(User.user == currentuser.email())
-            getresult = qry.fetch()
-
-            if getresult:
+            find = User.get_by_id(currentuser.user_id())
+            if find:
                 print("User Exists")
-
             else:
-                u = User(user=currentuser.email(), name=currentuser.nickname(), index=currentuser.email().lower())
-                u.put()
+                guid = str(uuid4())
+                user = User(userid=currentuser.user_id(), email=currentuser.email(), name=guid,
+                            index=guid.lower())
+                user.key = ndb.Key(User, currentuser.user_id())
+                user.put()
 
         else:
             greeting = "<a href='%s'>Log In </a>" % users.create_login_url('/')
 
         qry = Image.query().order(-Image.uploaded)
-        images = qry.fetch()
+        images = qry.fetch(10)
 
         template_values = {
             "upload_url": upload_url,
@@ -80,6 +92,7 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         upload = self.get_uploads()[0]
         image = Image(user=users.get_current_user().user_id(), blob_key=upload.key())
+        image.tags = []
         image.put()
         self.redirect('/')
 
@@ -88,7 +101,10 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, resource):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
-        self.send_blob(blob_info)
+        self.response.headers["Content-Type"] = "image/png"
+        self.response.headers.add_header("Max-Age", "604800")
+
+        self.send_blob(blob_info, save_as=True)
 
 
 class UserHandler(webapp2.RequestHandler):
